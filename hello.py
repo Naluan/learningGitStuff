7214afc2e0889c2fbb2f084f54dc28277bd8ebdd
Luan Nguyen <ngunalu@gmail.com>
2025-01-07 16:36:19 +0100

    added differ

---
+++ hello.py
@@ -0,0 +1,13 @@
+def summer(a: float, b: float) -> int:
+    c = a + b
+    print(a + b)
+    return c
+
+def differ(a, b):
+    print(a - b)
+
+
+num1:float = 2.3  
+num2:float = 4.3
+result = summer(num1, num2)
+print(result)
